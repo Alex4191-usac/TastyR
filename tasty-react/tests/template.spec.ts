47bfeca57455ac5502fc973fe30689bb9c995f47
Alex4191-usac <3444601420101@ingenieria.usac.edu.gt>
2024-05-14 21:06:47 -0600

    se agrego test con url de vercel

--- tasty-react/tests/template.spec.ts
+++ tasty-react/tests/template.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect, type Page } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173/');
+    await page.goto('https://tasty-r-rsc4.vercel.app');
 });
 
 //end to end test
@@ -19,6 +19,22 @@ test.describe('navigate into categories', () => {
             await expect(button).toBeVisible();
         }
     });
+
+    test('should navigate into a category and click on a recipe', async ({ page }) => {
+        await page.click('text=Beef');
+        const pageNumber = Math.floor(Math.random() * 3) + 1;
+        await page.click(`button >> text=${pageNumber}`);
+        const gridContainer = await page.locator('section >> div');
+        const firstChild = await gridContainer.locator('div').first();
+        await firstChild.hover();
+        await page.click('text=View Recipe');
+        expect(page.url()).toContain('recipe');
+    });
+
+    
+    
+
+
 
 
     
